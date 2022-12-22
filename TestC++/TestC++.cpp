c3278244d2782ee9ff8ed4e0df14eb12d8d89146
true <true@OLEDINATOR-5000>
2022-12-22 11:38:11 +0300

    Add project files.

---
+++ TestC++/TestC++.cpp
@@ -0,0 +1,11 @@
+// TestC++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    cout << "----------Hello, my name id Adel.-------\n";//Prints out "Hello, my name id Adel." then starts a new line.
+    cout << "-----My university ID is: 441009021-----";//Prints out "My university ID is: 441009021".
+}
